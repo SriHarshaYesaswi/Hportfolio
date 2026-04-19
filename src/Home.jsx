71020b263ed6a673d3e21c79838d643348c1c8f2
nikshith-y <nikshith.06@gmail.com>
2026-04-19 20:44:11 +0530

    star

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -11,12 +11,25 @@ import Contact from "./components/Contact";
 import { StarsCanvas } from "./components/canvas";
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handler = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener("change", handler);
+    return () => mediaQuery.removeEventListener("change", handler);
+  }, []);
+
   return (
     <div className="relative z-0 bg-[#050816]">
       <div className="relative z-0">
-        <div className="absolute inset-0 z-[-1] pointer-events-none">
-          <iframe src="/tt.html" className="w-full h-full border-none" title="Background" />
-        </div>
+        {!isMobile && (
+          <div className="absolute inset-0 z-[-1] pointer-events-none">
+            <iframe src="/tt.html" className="w-full h-full border-none" title="Background" />
+          </div>
+        )}
         <Navbar />
         <Hero />
       </div>
